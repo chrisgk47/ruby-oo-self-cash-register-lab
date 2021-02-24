2f0e1b56a814f4abcfffb7b60c07fa3930ad22b1
Christopher Kim <chris.kim1990@gmail.com>
2021-02-24 18:54:36 -0500

    Cash Register Lab

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,36 @@
+require 'pry'
+class CashRegister
+    attr_accessor :total, :discount, :item, :last_transaction
 
+    def initialize(discount = 0) 
+     @total = 0
+     @discount = discount
+     @item = []
+    end
+
+    def add_item(title, price, quantity = 1)
+        self.total += price * quantity
+        quantity.times do
+            item << title
+        end
+        self.last_transaction = price * quantity
+    end
+
+    def apply_discount
+        if @discount == 0
+            return "There is no discount to apply."
+        else
+            self.total = (total * ((100.0 - discount.to_f)/100)).to_i
+            return "After the discount, the total comes to $#{self.total}."
+        end
+    end
+       
+    def items
+        @item
+    end
+
+    def void_last_transaction
+        self.total = self.total - self.last_transaction
+    end
+
+end
